fd00b296abc8b85355884dcea53e8ac9e918bb5b
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-04-29 13:06:43 +0300

    useEffect Hook

--- src/DogPics.js
+++ src/DogPics.js
@@ -6,15 +6,13 @@ function DogPics() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect");
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
         setImages(data.message);
       });
-  });
-
+  }, []);
+  
   console.log("render");
 
   return (
